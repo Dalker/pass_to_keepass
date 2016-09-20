5bd023a7dc2e1d9a15bfe9bdd87288745edefa63
Pascal Wichmann <wichmannpas@gmail.com>
2016-09-20 11:40:45 +0200

    Passwords retrieved from pass

--- export.py
+++ export.py
@@ -9,6 +9,10 @@ variable PASSWORD_STORE_DIR (if set).
 """
 
 import os
+import subprocess
+
+from collections import Generator
+from keepassdb import Database
 
 # the path of the keepass file
 KEEPASS_PATH = os.path.join(os.getcwd(), 'out.kpd')
@@ -17,12 +21,35 @@ KEEPASS_PATH = os.path.join(os.getcwd(), 'out.kpd')
 KEEPASS_PW = 'keepass_pw'
 
 # the path inside the pass to export (empty means export all)
-EXPORT = 'subpath-to-export'
+EXPORT_SUBPATH = 'subpath-to-export/'
 
 
 def export_passwords():
     """Export the passwords."""
-    return
+    pass_base_path = _get_pass_base_path()
+    export_path = _get_pass_export_path(pass_base_path)
+
+    for full_password_path in _recursive_list_pass_dir(export_path):
+        pass_path = full_password_path.replace(pass_base_path, '', 1)
+        pass_path = _remove_leading_slash(pass_path)
+        password = _get_password_from_pass(pass_path)
+        print(pass_path)
+        print(password)
+
+
+def _get_password_from_pass(pass_path) -> str:
+    """Get the pasword from pass."""
+    # [:-1] to remove trailing newline
+    return subprocess.check_output(['pass', pass_path]).decode()[:-1]
+
+
+def _recursive_list_pass_dir(base_dir: str) -> Generator:
+    """List a directory recursively."""
+    for path, dirs, files in os.walk(base_dir):
+        for file in files:
+            if file[-4:] != '.gpg':
+                continue
+            yield os.path.join(path, file[:-4])
 
 
 def _get_pass_base_path() -> str:
@@ -31,5 +58,17 @@ def _get_pass_base_path() -> str:
         '~/.password-store'))
 
 
+def _get_pass_export_path(base_path: str) -> str:
+    """Get the export path."""
+    return os.path.join(base_path, EXPORT_SUBPATH)
+
+
+def _remove_leading_slash(string: str) -> str:
+    """Remove a trailing slash (if any)."""
+    if string[0] != '/':
+        return string
+    return string[1:]
+
+
 if __name__ == '__main__':
-    print(_get_pass_base_path())
+    export_passwords()
