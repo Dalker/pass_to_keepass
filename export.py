ab01493fe741fa74ca6b9e312fa1b5a6a0495c7a
Pascal Wichmann <wichmannpas@gmail.com>
2016-09-20 11:48:09 +0200

    Export fully working

--- export.py
+++ export.py
@@ -10,12 +10,13 @@ variable PASSWORD_STORE_DIR (if set).
 
 import os
 import subprocess
+import sys
 
 from collections import Generator
 from keepassdb import Database
 
 # the path of the keepass file
-KEEPASS_PATH = os.path.join(os.getcwd(), 'out.kpd')
+KEEPASS_PATH = os.path.join(os.getcwd(), 'out.kdb')
 
 # the location of the password to encrypt the keepass file with in pass
 KEEPASS_PW = 'keepass_pw'
@@ -26,15 +27,26 @@ EXPORT_SUBPATH = 'subpath-to-export/'
 
 def export_passwords():
     """Export the passwords."""
+    if os.path.isfile(KEEPASS_PATH) and '-f' not in sys.argv:
+        print('Out file exists and -f not used.')
+        sys.exit(1)
+
     pass_base_path = _get_pass_base_path()
     export_path = _get_pass_export_path(pass_base_path)
+
+    db = Database()
+    group = db.create_group(title='PASS(1)')
 
     for full_password_path in _recursive_list_pass_dir(export_path):
         pass_path = full_password_path.replace(pass_base_path, '', 1)
         pass_path = _remove_leading_slash(pass_path)
         password = _get_password_from_pass(pass_path)
-        print(pass_path)
-        print(password)
+        group.create_entry(
+            title=pass_path, url=pass_path, username=pass_path,
+            password=password)
+
+    db.save(KEEPASS_PATH, password=_get_password_from_pass(KEEPASS_PW))
+    print('Export successful.')
 
 
 def _get_password_from_pass(pass_path) -> str:
